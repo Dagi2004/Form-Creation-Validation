d4e8bc38f5762d9ee2dbb90d4ea8b1934fa025d5
Dagi <dagmawimilkias@gmail.com>
2024-07-17 16:33:13 +0300

    Fetch data from API

---
+++ fetch-data.js
@@ -0,0 +1,20 @@
+async function fetchUserData() {
+  const dataContainer = document.getElementById("api-data");
+  const apiurl = "https://jsonplaceholder.typicode.com/users";
+  try {
+    const response = await fetch(apiurl);
+    const users = await response.json();
+    dataContainer.innerHTML = "";
+    const userList = document.createElement("ul");
+    users.forEach((user) => {
+      const userItem = document.createElement("li");
+      userItem.textContent = user.name;
+      userList.appendChild(userItem);
+    });
+    dataContainer.appendChild(userList);
+  } catch (error) {
+    dataContainer.innerHTML = "";
+    dataContainer.textContent = "Failed to load user data";
+  }
+}
+document.addEventListener("DOMContentLoaded", fetchUserData);
